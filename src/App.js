dd562b426c69648e097cf849f975cbb12e57336d
KedarKK1 <officialkedark1@gmail.com>
2022-07-12 23:03:50 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "antd/dist/antd.css"; //important to import this line otherwise it will not show antd css
+import "antd/dist/antd.css"; 
 import Homepage from './pages/homePage/Homepage.js';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
